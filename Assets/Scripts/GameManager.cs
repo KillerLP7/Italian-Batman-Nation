baad68ff77daeaf88801ce743edc93407e4d8b13
KillerLP7 <107851117+KillerLP7@users.noreply.github.com>
2024-01-18 09:20:53 +0100

    15

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,7 +21,7 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI cooldownNumber;
 
     private Vector3 playerPos;
-    private int waveNumber;
+    private int waveNumber = 15;
     public int hp;
     private int activeEnemies;
     private bool endOfWave;
