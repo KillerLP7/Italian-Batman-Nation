41607e5ee34e77ed514efb703095aeef7787f4e4
KillerLP7 <107851117+KillerLP7@users.noreply.github.com>
2024-01-18 09:44:33 +0100

    ToxicDmg added

--- Assets/Scripts/Toxic.cs
+++ Assets/Scripts/Toxic.cs
@@ -6,19 +6,25 @@ using UnityEngine;
 public class Toxic : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anime;
     [SerializeField] private GameObject attack;
     private float counter;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anime = GetComponent<Animator>();
+        anime.SetBool("ToxicBallSpawn", true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        anime.SetBool("ToxicBallSpawn", false);
+        anime.SetBool("ToxicDmg", false);
         rb.velocity = new Vector2(-2, 0);
         if (counter > 0.5)
         {
+            anime.SetBool("ToxicDmg", true);
             Instantiate(attack, transform.position, Quaternion.identity);
             counter = 0;
         }
